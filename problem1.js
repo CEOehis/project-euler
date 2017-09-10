1c29ecd148da8a2697f00570c6fb77927e7a81f9
Celestine Ekoh-Ordan <ceo.ehis@outlook.com>
2017-09-10 22:08:04 +0100

    efficient solution problem1.js

--- problem1.js
+++ problem1.js
@@ -1,20 +1,11 @@
-function getMultiples (num) {
+function sumMultiplesOfNum (num) {
   var result = []
   for (let i = 2; i < num; i++) {
     if ((i % 3 == 0) || (i % 5 == 0)) {
       result.push(i)
     }
   }
-  return result
-}
-
-function sumResult (arr) {
-  return arr.reduce(function (sum, value) {
+  return result.reduce(function (sum, value) {
     return sum + value
   })
-}
-
-function sumMultiplesOfNum (num) {
-  var multiples = getMultiples(num)
-  return sumResult(multiples)
 }
